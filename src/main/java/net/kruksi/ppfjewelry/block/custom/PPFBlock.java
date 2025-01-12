1960a1b1a38ccef6a37c054165fdf615983fe713
Kruksii <wowkruksi@gmail.com>
2025-01-12 15:24:04 +0100

    Add tooltips to items

--- src/main/java/net/kruksi/ppfjewelry/block/custom/PPFBlock.java
+++ src/main/java/net/kruksi/ppfjewelry/block/custom/PPFBlock.java
@@ -1,5 +1,9 @@
 package net.kruksi.ppfjewelry.block.custom;
 
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.BlockState;
@@ -13,6 +17,8 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
+
+import java.util.List;
 
 public class PPFBlock extends Block {
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
@@ -44,5 +50,11 @@ public class PPFBlock extends Block {
 
     public BlockState mirror(BlockState state, Mirror mirrorIn) {
         return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        tooltipComponents.add(Component.translatable("tooltip.ppfjewelry.ppf_block"));
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 }
--- src/main/java/net/kruksi/ppfjewelry/items/custom/JewelfileItem.java
+++ src/main/java/net/kruksi/ppfjewelry/items/custom/JewelfileItem.java
@@ -1,16 +1,21 @@
 package net.kruksi.ppfjewelry.items.custom;
 
 import net.kruksi.ppfjewelry.block.ModBlock;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 
+import java.util.List;
 import java.util.Map;
 
 public class JewelfileItem extends Item {
@@ -42,5 +47,17 @@ public class JewelfileItem extends Item {
         }
 
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        if(Screen.hasShiftDown()) {
+            tooltipComponents.add(Component.translatable("tooltip.ppfjewelry.jewelfile_shift"));
+        } else {
+            tooltipComponents.add(Component.translatable("tooltip.ppfjewelry.jewelfile"));
+            tooltipComponents.add(Component.translatable("tooltip.ppfjewelry.more"));
+        }
+
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 }
